73ca233e033ea3f0996c4a40c1059bc461b05802
Tyler Kneisly <tkneisly@fullsail.edu>
2012-09-13 23:01:13 -0400

    Final Commit for Week 2

---
+++ Week_2/xhr/json.js
@@ -0,0 +1,15 @@
+{
+	"item": [
+				{
+				"groups": 		["Group: Online"],
+				"titles": 		["Title:  Neuro Science"],
+				"authors": 		["Author:  Walder Jennings"],
+				"readpages": 	["Pages:  786"],
+				"datefinished": ["Date Finished:  2008-06-19"],
+				"rating": 		["Rating:  10"],
+				"category": 	["Genre:  Non-Fiction"],
+				"favs": 		["Favorite:  No"],
+				"note": 		["Notes:  Apparently, what's on your mind matters."]
+				}
+			]
+}
